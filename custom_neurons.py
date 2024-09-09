13dee8b1a269c1d607c5bc1ba6491e11c89e17a2
Kamran Vatankhah <kamran.vat@protonmail.com>
2024-09-09 17:36:48 +0200

    added experimental clustering methods

--- custom_neurons.py
+++ custom_neurons.py
@@ -51,10 +51,59 @@ class CustomLeaky(snn.Leaky):
         self.coordinates[fired_indices[1]] += 0.01 * (fired_coordinates - center)
 
     def cluster_neurons_batchwise(self, spk):
-        pass
+        # 0. move all neuron coordinates outward from the center
+        # 1. remove any neurons that did not fire
+        # 2. analyze (from the full batch of neurons) which ones tend to fire together
+
+        # 3. move the coordinates of the neurons that tend to fire together closer to each other
+
+        # move all neuron coordinates outward from the center
+        center = torch.mean(self.coordinates, dim=0)
+        self.coordinates += 0.01 * (self.coordinates - center)
+
+        # remove any neurons that did not fire (TODO: check if this is necessary)
+
+        # analyze (from the full batch of neurons) which ones tend to fire together
+        correlation_matrix = torch.zeros(self.num_neurons, self.num_neurons)
+        for i in range(spk.shape[0]):
+            fired_indices = torch.nonzero(spk[i], as_tuple=True)[1]
+            if len(fired_indices) == 0:
+                return
+            for j in range(len(fired_indices)):
+                for k in range(j + 1, len(fired_indices)):
+                    neuron1 = fired_indices[j]
+                    neuron2 = fired_indices[k]
+                    correlation_matrix[neuron1, neuron2] += 1
+                    correlation_matrix[neuron2, neuron1] += 1
+
+        # move the coordinates of the neurons that tend to fire together closer to each other. use the information from the correlation matrix
+        for i in range(self.num_neurons):
+            for j in range(i + 1, self.num_neurons):
+                if correlation_matrix[i, j] > 0:
+                    self.coordinates[i] += (
+                        0.01
+                        * correlation_matrix[i, j]
+                        * (self.coordinates[j] - self.coordinates[i])
+                    )
+                    self.coordinates[j] += (
+                        0.01
+                        * correlation_matrix[i, j]
+                        * (self.coordinates[i] - self.coordinates[j])
+                    )
 
     def cluster_neurons_stepwise(self, spk):
-        pass
+        # 1. remove any neurons that did not fire
+        # 2. go stepwise (for batch_size steps) over the neurons that fired, each time, do one step of cluster_neurons_simple
+        # 3. repeat for the next batch_size neurons that fired
+
+        # remove any neurons that did not fire (TODO: check if this is necessary)
+        fired_indices = torch.nonzero(spk, as_tuple=True)[1]
+        if len(fired_indices) == 0:
+            return
+        
+        # go stepwise (for len(fired_indices) steps) over the neurons that fired, each time, do one step of cluster_neurons_simple
+        for i in range(len(fired_indices)):
+            self.cluster_neurons_simple(spk[:, fired_indices[i]])
 
 
     def export_positions_history(self, file_path):
