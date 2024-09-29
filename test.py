8d1632fb617ada2a6b6d865e71d2efe5bf675b94
Kamran Vatankhah <kamran.vat@protonmail.com>
2024-09-29 17:27:38 +0200

    moved heatmap accumulation to model

--- test.py
+++ test.py
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 def test(net, test_loader, device):
     total = 0
     correct = 0
-    total_heatmap1 = torch.zeros((net.fc1.out_features,), device=device)
-    total_heatmap2 = torch.zeros((net.fc2.out_features,), device=device)
 
     with torch.no_grad():
         net.eval()
@@ -19,20 +17,18 @@ def test(net, test_loader, device):
             data = data.to(device)
             targets = targets.to(device)
 
-            test_spk, _, [heatmap1, heatmap2] = net(data.view(data.size(0), -1))
+            test_spk, _ = net(data.view(data.size(0), -1))
 
             _, predicted = test_spk.sum(dim=0).max(1)
             total += targets.size(0)
             correct += (predicted == targets).sum().item()
 
-            total_heatmap1 += heatmap1
-            total_heatmap2 += heatmap2
 
             net.set_record_heatmap(False)
 
     visualize_neuron_positions(net)
 
-    return total, correct, total_heatmap1, total_heatmap2
+    return total, correct
 
 
 def visualize_neuron_positions(net):
