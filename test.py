32c92ee0b81afe52fd6b2df4674fd39b4caaedaf
Kamran Vatankhah <kamran.vat@protonmail.com>
2024-09-30 15:14:50 +0200

    added input layer to network export

--- test.py
+++ test.py
@@ -28,20 +28,44 @@ def test(net, test_loader, device, max_steps=None):
             total += targets.size(0)
             correct += (predicted == targets).sum().item()
 
-
             net.record_heatmap = False
 
-    network_representation = NetworkRepresentation(net.export_representation())
-    heatmaps = network_representation.heatmaps
+    layers, weight_matrices, heatmaps = net.export_model_structure()
+    network_representation = NetworkRepresentation(layers, weight_matrices, heatmaps)
 
     visualize_neuron_positions(net)
     visualize_heatmaps(heatmaps)
 
+    network_representation.export_representation("network_representation.json")
+
     return total, correct
 
-def record(net, test_loader, device, max_steps=None):
-    # TODO build function that records spikes for N steps
-    pass
+def record(net, test_loader, device, record_batches=None):
+    with torch.no_grad():
+        net.eval()
+        net.record_spike_times = True
+        for data, targets in test_loader:
+
+            data = data.to(device)
+            targets = targets.to(device)
+
+            test_spk, _ = net(data.view(data.size(0), -1))
+
+            _, predicted = test_spk.sum(dim=0).max(1)
+
+            net.record_heatmap = False
+
+    network_representation = NetworkRepresentation(net.export_model_structure())
+
+    # Debugging:
+    heatmaps = network_representation.heatmaps
+    visualize_neuron_positions(net)
+    visualize_heatmaps(heatmaps)
+
+    activations = net.get_activations # TODO maybe rewrite networkRepresentation class to just get the net at init
+    network_representation.export_activations(activations, "network_activations.json")
+
+
 
 def cluster_simple(net, test_loader, device, max_steps=None):
     with torch.no_grad():
