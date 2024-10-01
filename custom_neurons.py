338ef6b97112d1596d95eda60f32d2cafac12c32
Kamran Vatankhah <kamran.vat@protonmail.com>
2024-10-01 13:31:11 +0200

    built test_clustering.py to verify cluster_neurons_simple functionality, improved plotting

--- custom_neurons.py
+++ custom_neurons.py
@@ -33,9 +33,13 @@ class CustomLeaky(snn.Leaky):
         )
 
     def cluster_neurons_simple(self, spk, factor=0.01):
+        # TODO verify that this works
         """Looks at all neurons that fired in spk (can be a timestep or whole batch) and moves them closer to each other."""
         # get indices of neurons that fired
         fired_indices = torch.nonzero(spk, as_tuple=True)
+
+        # get sum of spk over batch dimension
+        spk_sum = torch.sum(spk, dim=0)
 
         # TODO spk shape is always [batch size, num_neurons of next layer]
         # fired_indices[0].shape == fired_indices[1].shape
@@ -44,96 +48,14 @@ class CustomLeaky(snn.Leaky):
 
         fired_coordinates = self.coordinates[fired_indices[1]]
 
-        # move all neurons away from the center a bit
-        center = torch.mean(fired_coordinates, dim=0)
-        self.coordinates += factor * (self.coordinates - center)
+        # move each neuron that fired (factor*spk_sum[neuron coordinates]) closer to the average of all neurons that fired
+        self.coordinates[fired_indices[1]] += factor * spk_sum[fired_indices[1]].reshape(
+            -1, 1
+        ) * (torch.mean(fired_coordinates, dim=0) - fired_coordinates)
 
-        # move all neurons that fired closer to each other
-        center = torch.mean(fired_coordinates, dim=0)
-        self.coordinates[fired_indices[1]] += factor * (fired_coordinates - center)
-
-    def cluster_neurons_simple_stepwise(self, spk):
-        """go stepwise (for batch_size steps) over the neurons that fired, each time, do one step of cluster_neurons_simple"""
-        fired_indices = torch.nonzero(spk, as_tuple=True)[1]
-        if len(fired_indices) == 0:
-            return
-
-        # go stepwise (for len(fired_indices) steps) over the neurons that fired, each time, do one step of cluster_neurons_simple
-        for i in range(len(fired_indices)):
-            self.cluster_neurons_simple(spk[:, fired_indices[i]])
-
-    def cluster_neurons_corr_matrix(self, spk):
-        """Iterates through the batch, clustering neurons that fired together at each timestep."""
-        # this moves those that fired together multiple times in a batch closer to each other than the ones that fired fewer times
-        # should be a similar result to cluster_neurons_simple_stepwise, but might be more efficient
-
-        # move all neuron coordinates outward from the center
+        # move all neurons away from the center by the average amount that they moved towards the center
         center = torch.mean(self.coordinates, dim=0)
-        self.coordinates += 0.01 * (self.coordinates - center)
-
-        # analyze (from the full batch of neurons) which ones tend to fire together
-        correlation_matrix = torch.zeros(self.num_neurons, self.num_neurons)
-        for i in range(spk.shape[0]):
-            fired_indices = torch.nonzero(spk[i], as_tuple=True)[1]
-            if len(fired_indices) == 0:
-                return
-            for j in range(len(fired_indices)):
-                for k in range(j + 1, len(fired_indices)):
-                    neuron1 = fired_indices[j]
-                    neuron2 = fired_indices[k]
-                    correlation_matrix[neuron1, neuron2] += 1
-                    correlation_matrix[neuron2, neuron1] += 1
-
-        # move the coordinates of the neurons that tend to fire together closer to each other. use the information from the correlation matrix
-        for i in range(self.num_neurons):
-            for j in range(i + 1, self.num_neurons):
-                if correlation_matrix[i, j] > 0:
-                    self.coordinates[i] += (
-                        0.01
-                        * correlation_matrix[i, j]
-                        * (self.coordinates[j] - self.coordinates[i])
-                    )
-                    self.coordinates[j] += (
-                        0.01
-                        * correlation_matrix[i, j]
-                        * (self.coordinates[i] - self.coordinates[j])
-                    )
-
-    def update_connections(self, spk, input_spk, weight_matrix):
-        """Updates the connections between neurons based on the spikes at the current timestep."""
-        # Get indices of neurons that fired in the current layer and the one before
-        curr_indices = torch.nonzero(spk, as_tuple=True)[1]
-        input_indices = torch.nonzero(input_spk, as_tuple=True)[1]
-    
-        # Ensure that the weight matrix dimensions match the input and current layer sizes
-        assert weight_matrix.shape[0] == input_spk.shape[1], "Weight matrix row size must match input layer size"
-        assert weight_matrix.shape[1] == spk.shape[1], "Weight matrix column size must match current layer size"
-
-        # Handle empty tensors
-        if curr_indices.numel() == 0 or input_indices.numel() == 0:
-            return
-        
-        # Validate indices
-        if curr_indices.max() >= weight_matrix.shape[1] or input_indices.max() >= weight_matrix.shape[0]:
-            raise IndexError("Index out of bounds for weight matrix dimensions")
-    
-        # Create a mask for active connections (from non-zero weights)
-        active_connections = weight_matrix[input_indices][:, curr_indices] != 0
-    
-        # Get the indices of active connections
-        active_input_indices, active_curr_indices = torch.nonzero(active_connections, as_tuple=True)
-    
-        # Map the indices back to the original neuron indices
-        active_input_indices = input_indices[active_input_indices]
-        active_curr_indices = curr_indices[active_curr_indices]
-
-        # Update connections based on the active input neurons
-        for input_idx, curr_idx in zip(active_input_indices, active_curr_indices):
-            connection = (input_idx.item(), curr_idx.item())
-            if connection in self.connections:
-                self.connections[connection] += 1
-            else:
-                self.connections[connection] = 1
+        self.coordinates += factor * (center - self.coordinates) * spk_sum.shape[0]
 
 
     # TODO build this:
