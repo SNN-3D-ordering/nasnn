fixed manhattan distance calculation, looks at correct index now

custom_neurons.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 import json
 from utils import map_to_2d_grid_row_wise
+from utils import map_to_1d_list_row_wise
 from utils import convert_layer_size_to_grid_size
 
 
@@ -84,7 +85,6 @@ class CustomLeaky(snn.Leaky):
         if heat != 0:
             self.coordinates += torch.randn_like(self.coordinates) * heat
 
-
     def export_positions_history(self, file_path):
         """
         Exports the positions history to a JSON file.
@@ -114,10 +114,18 @@ class CustomLeaky(snn.Leaky):
             np.ndarray: 2d grid with neuron indices.
         """
         coordinates = self.coordinates.cpu().numpy()
-        grid = map_to_2d_grid_row_wise(
-            coordinates, self.grid_dims
-        )  # TODO try out other mapping functions
+        grid = map_to_2d_grid_row_wise(coordinates, self.grid_dims)
         return grid
+
+    def return_1d_list(self):
+        """
+        Maps the 2D coordinates to a 1D list.
+
+        Returns:
+            np.ndarray: 1d list with neuron indices.
+        """
+        coordinates = self.coordinates.cpu().numpy()
+        return map_to_1d_list_row_wise(coordinates) # TODO remove
 
     def return_connections(self):
         """

main.py
@@ -104,9 +104,9 @@ if args.eval:
     net.load_state_dict(torch.load(config["filepaths"]["model_filepath"]))
 
     # Evaluate model
-    #print("Evaluating model accuracy...")
-    #total, correct = test(net, test_loader, device, config)
-    #print(f"Accuracy: {correct/total*100:.2f}%")
+    print("Evaluating model accuracy...")
+    total, correct = test(net, test_loader, device, config)
+    print(f"Accuracy: {correct/total*100:.2f}%")
 
     # Measure network
     print("Measuring network distance...")

metric.py
@@ -1,7 +1,7 @@
 import json
 from tqdm import tqdm
 from utils import index_to_coord, manhattan_distance_2d
-
+import numpy as np
 
 def calculate_distance_between_layers(data, layer_idx):
     """
@@ -21,9 +21,12 @@ def calculate_distance_between_layers(data, layer_idx):
                     current and next layers.
     """
     heatmaps = data["heatmaps"]
-    weight_matrices = data["weight_matrices"]
+    weight_matrices = data["weight_matrices"] # TODO load only current one and next one
     layer_shapes = data["metadata"]["layer_dimensions"]
 
+    current_layer = np.array(data["layers"][layer_idx]).flatten()
+    next_layer = np.array(data["layers"][layer_idx + 1]).flatten()
+    
     current_layer_heatmap = heatmaps[layer_idx]
     next_layer_heatmap = heatmaps[layer_idx + 1]
     weight_matrix = weight_matrices[layer_idx]
@@ -53,23 +56,69 @@ def calculate_distance_between_layers(data, layer_idx):
 
     layer_distance = 0
 
+    # TODO instead of iterating over layer size, iterate over the current layer and take the values that are in there as indices for where to look in the heatmap
+    # for n_idx in tqdm(
+    #     range(num_neurons_current_layer),
+    #     total=num_neurons_current_layer,
+    #     desc=f"Layer {layer_idx} neurons",
+    # ):
+    #     if current_layer_heatmap[n_idx] == 0:
+    #         continue  # Skip padded neurons in the current layer
+
+    #     n_coord = index_to_coord(n_idx, current_shape)
+    #     for m_idx in range(
+    #         num_neurons_next_layer
+    #     ):  # Loop through valid neurons in the next layer
+    #         if next_layer_heatmap[m_idx] == 0:
+    #             continue  # Skip padded neurons in the next layer
+
+    #         m_coord = index_to_coord(m_idx, next_shape)
+
+    #         # Adjust m_coord to align centers of different grid sizes
+    #         adjusted_m_coord = (
+    #             m_coord[0] + offset_current_to_next[0],
+    #             m_coord[1] + offset_current_to_next[1],
+    #         )
+
+    #         # Get the weight from the matrix
+    #         weight = (
+    #             weight_matrix[m_idx][n_idx]
+    #             if m_idx < len(weight_matrix) and n_idx < len(weight_matrix[m_idx])
+    #             else 0
+    #         )
+
+    #         # Only consider non-zero weights
+    #         if weight != 0:
+    #             grid_distance = manhattan_distance_2d(n_coord, adjusted_m_coord)
+    #             distance = (
+    #                 1 + grid_distance
+    #             )  # Vertical distance + grid distance, assume the vertical distance between layers is 1
+    #             layer_distance += (
+    #                 current_layer_heatmap[n_idx] * distance
+    #             )  # Neurons that are more active contribute more to the distance
+
+    # n_idx and m_idx are the values stored in the layer, which represent neuron indices.
+    # n_grid_idx and m_grid_idx are the indices of those values in the layer.
+    n_grid_idx = 0
+    m_grid_idx = 0
+
     for n_idx in tqdm(
-        range(num_neurons_current_layer),
+        current_layer,
         total=num_neurons_current_layer,
         desc=f"Layer {layer_idx} neurons",
     ):
         if current_layer_heatmap[n_idx] == 0:
             continue  # Skip padded neurons in the current layer
 
-        n_coord = index_to_coord(n_idx, current_shape)
-        for m_idx in range(
-            num_neurons_next_layer
-        ):  # Loop through valid neurons in the next layer
+        n_coord = index_to_coord(n_grid_idx, current_shape)
+        n_grid_idx += 1
+
+        for m_idx in next_layer:  # Loop through valid neurons in the next layer
             if next_layer_heatmap[m_idx] == 0:
                 continue  # Skip padded neurons in the next layer
 
-            m_coord = index_to_coord(m_idx, next_shape)
-
+            m_coord = index_to_coord(m_grid_idx, next_shape)
+            m_grid_idx += 1
             # Adjust m_coord to align centers of different grid sizes
             adjusted_m_coord = (
                 m_coord[0] + offset_current_to_next[0],
@@ -114,7 +163,7 @@ def calculate_total_distance(data):
     for layer_idx in range(num_layers - 1):
         layer_distance = calculate_distance_between_layers(data, layer_idx)
         print(
-            f"Total weighted Manhattan distance between layer {layer_idx} and layer {layer_idx + 1}: {layer_distance}"
+            f"Total weighted Manhattan distance between layers {layer_idx} and {layer_idx + 1}: {layer_distance}"
         )
         total_distance += layer_distance
 

test.py
@@ -69,6 +69,7 @@ def record(net, test_loader, device, config, record_batches=None):
     network_representation = NetworkRepresentation(layers, weight_matrices, heatmaps)
 
     # Debugging:
+    print(heatmaps.shape)
     heatmaps = network_representation.heatmaps
     visualize_neuron_positions_color(net.lif1)
     visualize_heatmaps(heatmaps)
@@ -85,9 +86,10 @@ def cluster_simple(net, test_loader, device, config, max_steps=None):
     # TODO let this load a specified nr of samples up to test_loader max amount (print message)
     with torch.no_grad():
         net.eval()
-        print("debugging: setting cluster_simple to True")
         net.set_cluster_simple(True)
 
+        # debugging: print first 10 values from first heatmap
+        print("Heatmap before clustering: ", net.heatmaps[1][:10])
         for step, (data, targets) in enumerate(test_loader):
             if max_steps is not None and step >= max_steps:
                 break
@@ -104,6 +106,7 @@ def cluster_simple(net, test_loader, device, config, max_steps=None):
 
         print("Clustering done. Writing...")
         layers, weight_matrices, heatmaps = net.export_model_structure()
+        print("Heatmap after clustering: ", heatmaps[1][:10])
         network_representation = NetworkRepresentation(layers, weight_matrices, heatmaps)
         export_filepath = config["filepaths"]["network_representation_filepath"]
         network_representation.export_representation(export_filepath)

utils.py
@@ -77,7 +77,7 @@ def map_to_2d_grid_row_wise(coordinates, grid_size):
         np.ndarray: 2D grid with neuron indices.
     """
     num_neurons = coordinates.shape[0]
-    sorted_indices = np.argsort(coordinates[:, 0])  # Sort by x-coordinate
+    sorted_indices = np.argsort(coordinates[:, 0])  # Sort by x-coordinate TODO build proper sorting
     grid = np.zeros(grid_size, dtype=int) - 1  # Initialize grid with -1 (empty)
 
     for idx, neuron_idx in enumerate(sorted_indices):
@@ -86,6 +86,20 @@ def map_to_2d_grid_row_wise(coordinates, grid_size):
         grid[row, col] = neuron_idx
 
     return grid
+
+
+def map_to_1d_list_row_wise(coordinates):
+    """
+    Maps 2D coordinates to a 1D list row-wise.
+
+    Args:
+        coordinates (np.ndarray): Array of 2D coordinates.
+
+    Returns:
+        np.ndarray: 1D list with neuron indices.
+    """
+    sorted_indices = np.argsort(coordinates[:, 0])  # Sort by x-coordinate TODO build proper sorting
+    return sorted_indices
 
 
 def get_next_square_numbers(number):